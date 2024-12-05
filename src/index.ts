6887ef92d8a438567a95e3cf671e9dac91e7b08e
Munawwar <munawwarfiroz@hotmail.com>
2024-12-05 12:12:58 +0400

    improved get() path resolution

--- src/index.ts
+++ src/index.ts
@@ -193,113 +193,125 @@ export function reverse<T>(arr: T[]): T[] {
 /**
  * Get multiple object or array value by paths. Example path: `a[0].b.c`
  */
-export function at(
-	source: Record<string, unknown> | any[],
-	paths: string[] | string,
-) {
-	const out = [];
+export const at = (() => {
+	const pathSplitRegex = /(?:\.|(?:\[['"]?))|(?:['"]?\])/;
+	return function at(
+		source: Record<string, unknown> | any[],
+		paths: string[] | string,
+	) {
+		const out = [];
 
-	if (typeof paths === "string") {
-		paths = [paths];
-	}
+		if (typeof paths === "string") {
+			paths = [paths];
+		}
 
-	for (let i = 0; i < paths.length; i++) {
-		const path: string = paths[i] || "";
-		const parts = path.split(/[.[\]]/g);
-		let haystack: any = source;
-		let found = true;
-		for (let i = 0; i < parts.length; i++) {
-			const part = parts[i];
-			if (part === "") continue;
-			if (!(part in haystack)) {
-				found = false;
-				break;
+		for (let i = 0; i < paths.length; i++) {
+			const path: string = paths[i] || "";
+			const parts = String(path).split(pathSplitRegex);
+			let haystack: any = source;
+			let found = true;
+			for (let i = 0; i < parts.length; i++) {
+				const part = parts[i];
+				if (part === "") continue;
+				if (!(part in haystack)) {
+					found = false;
+					break;
+				}
+				haystack = haystack[part];
 			}
-			haystack = haystack[part];
+
+			if (found) {
+				out.push(haystack);
+			}
 		}
 
-		if (found) {
-			out.push(haystack);
-		}
-	}
-
-	return out;
-}
+		return out;
+	};
+})();
 
 /**
  * Return item by path or default value if not present
  */
-export function get<T = any>(
-	source: Record<string, unknown> | any[],
-	path: string | number | Array<string | number>,
-	defaultValue?: T,
-): T | undefined {
-	if (Array.isArray(path)) {
-		path = path.join(".");
-	}
+export const get = (() => {
+	const pathSplitRegex = /(?:\.|(?:\[['"]?))|(?:['"]?\])/;
+	return function get<T = any>(
+		source: Record<string, unknown> | any[],
+		path: string | number | Array<string | number>,
+		defaultValue?: T,
+	): T | undefined {
+		if (Array.isArray(path)) {
+			path = path.join(".");
+		}
 
-	const parts = String(path).split(/[.[\]]/g);
-	let haystack: any = source;
-	for (let i = 0; i < parts.length; i++) {
-		const part = parts[i];
-		if (part === "") continue;
-		if (!(part in haystack)) return defaultValue;
-		haystack = haystack[part];
-	}
+		const parts = String(path).split(pathSplitRegex);
+		let haystack: any = source;
+		for (let i = 0; i < parts.length; i++) {
+			const part = parts[i];
+			if (part === "") continue;
+			if (!(part in haystack)) return defaultValue;
+			haystack = haystack[part];
+		}
 
-	return haystack;
-}
+		return haystack;
+	};
+})();
 
-export function set<T = any>(
-	target: Record<string, unknown> | any[],
-	path: string | number | Array<string | number>,
-	value?: T,
-): typeof target {
-	if (Array.isArray(path)) {
-		path = path.join(".");
-	}
+export const set = (() => {
+	const pathSplitRegex = /(?:\.|(?:\[['"]?))|(?:['"]?\])/;
+	return function set<T = any>(
+		target: Record<string, unknown> | any[],
+		path: string | number | Array<string | number>,
+		value?: T,
+	): typeof target {
+		if (Array.isArray(path)) {
+			path = path.join(".");
+		}
 
-	const parts = String(path).split(/[.[\]]/g);
-	let obj: any = target || {};
-	for (let i = 0; i < parts.length; i++) {
-		const part = parts[i];
-		if (part === "") continue;
-		if (i === parts.length - 1) {
-			obj[part] = value;
-		} else {
-			if (!obj[part]) {
-				const isNumeric = /^\d+$/.test(String(part));
-				obj[part] = isNumeric ? [] : {};
+		const parts = String(path).split(pathSplitRegex);
+		let obj: any = target || {};
+		for (let i = 0; i < parts.length; i++) {
+			const part = parts[i];
+			if (part === "") continue;
+			if (i === parts.length - 1) {
+				obj[part] = value;
+			} else {
+				if (!obj[part]) {
+					const isNumeric = /^\d+$/.test(String(part));
+					obj[part] = isNumeric ? [] : {};
+				}
+				obj = obj[part];
 			}
-			obj = obj[part];
 		}
-	}
 
-	return target;
-}
+		return target;
+	};
+})();
 
-export function unset(
-	target: Record<string, unknown> | any[],
-	path: string | number | Array<string | number>,
-): typeof target {
-	if (Array.isArray(path)) {
-		path = path.join(".");
-	}
-
-	const parts = String(path).split(/[.[\]]/g);
-	let obj: any = target || {};
-	for (let i = 0; obj && i < parts.length; i++) {
-		const part = parts[i];
-		if (part === "") continue;
-		if (i === parts.length - 1) {
-			delete obj[part];
-		} else {
-			obj = obj[part];
+export const unset = (() => {
+	const pathSplitRegex = /(?:\.|(?:\[['"]?))|(?:['"]?\])/;
+	return function unset(
+		target: Record<string, unknown> | any[],
+		path: string | number | Array<string | number>,
+	): typeof target {
+		if (Array.isArray(path)) {
+			path = path.join(".");
 		}
-	}
 
-	return target;
-}
+		const parts = String(path).split(pathSplitRegex);
+		let obj: any = target || {};
+		for (let i = 0; obj && i < parts.length; i++) {
+			const part = parts[i];
+			if (part === "") continue;
+			if (i === parts.length - 1) {
+				delete obj[part];
+			} else {
+				obj = obj[part];
+			}
+		}
+
+		return target;
+	};
+})();
 
 const empty = {};
 /**
@@ -1035,7 +1047,7 @@ export const escape = (() => {
 	function escapeHtmlChar(key: keyof typeof htmlEscapes): string {
 		return htmlEscapes[key];
 	}
-	return function escapeClosure(string: string) {
+	return function escape(string: string) {
 		string = String(string);
 		return string && reHasUnescapedHtml.test(string)
 			? string.replace(reUnescapedHtml, escapeHtmlChar as (k: string) => string)
@@ -1057,7 +1069,7 @@ export const unescape = (() => {
 	function unescapeHtmlChar(key: keyof typeof htmlUnescapes): string {
 		return htmlUnescapes[key];
 	}
-	return function unescapeClosure(string: string) {
+	return function unescape(string: string) {
 		string = String(string);
 		return string && reHasEscapedHtml.test(string)
 			? string.replace(reEscapedHtml, unescapeHtmlChar as (k: string) => string)
