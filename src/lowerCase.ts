a740565c85a780e11f9e26835e079d5e9c45ca20
Munawwar <munawwarfiroz@hotmail.com>
2024-12-11 22:40:52 +0400

    v0.15.4 - it seems only function calls are not tree shaked
    e.g. RegExp(..) or Object.getPrototypeOf({}) are only tree shaked if they are isolated into a file
    whereas var regex = /t/g
    will get tree shaked because it is a primitive

--- src/lowerCase.ts
+++
@@ -1,8 +0,0 @@
-import { splitCasedWords } from "./splitCasedWords";
-
-/**
- * Splits pascal/camel/snake case words and lower cases them.
- */
-export function lowerCase(val: string) {
-	return splitCasedWords(val).trim().toLowerCase();
-}
--- src/upperCase.ts
+++
@@ -1,8 +0,0 @@
-import { splitCasedWords } from "./splitCasedWords";
-
-/**
- * Splits pascal/camel/snake case words and upper cases them.
- */
-export function upperCase(val: string) {
-	return splitCasedWords(val).trim().toUpperCase();
-}
